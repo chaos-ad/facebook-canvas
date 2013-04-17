dad395936b89e95e0417befdf27ae053e621284c
Anatoly Kanivetskiy <chaos.a.d88@gmail.com>
2013-04-17 19:18:35 +0200

    Redirect and play pages are now templates

--- src/facebook_canvas_handler.erl
+++ src/facebook_canvas_handler.erl
@@ -4,39 +4,23 @@
 -export([handle/2]).
 -export([terminate/3]).
 
-init(_Transport, Req, []) ->
-    {ok, Req, undefined}.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-handle(Req, State) ->
+-record(state, {endpoint}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+init(_Transport, Req, []) ->
     {{IP, Port}, _} = cowboy_req:peer(Req),
     Endpoint = lists:flatten(io_lib:format("~s:~B", [inet_parse:ntoa(IP), Port])),
-    lager:debug("Serving iframe to '~s'...", [Endpoint]),
-    try
-        case cowboy_req:qs_val(<<"code">>, Req) of
-            {undefined, _} ->
-                %% Need to login:
-                {ErrorReason, _} = cowboy_req:qs_val(<<"error_reason">>, Req),
-                case ErrorReason of
-                    <<"user_denied">> ->
-                        lager:debug("Serving iframe to '~s': user denied the login", [Endpoint]),
-                        {ok, login_page(Req), State};
-                    undefined ->
-                        {ok, PostVars, _} = cowboy_req:body_qs(Req),
-                        case proplists:get_value(<<"signed_request">>, PostVars) of
-                            undefined -> error(400);
-                            SignedRequest when is_binary(SignedRequest) ->
-                                _ = parse_signed_request(SignedRequest),
-                                lager:debug("Serving iframe to '~s': redirecting to auth page", [Endpoint]),
-                                {ok, login_page(Req), State}
-%                                 case proplists:get_value(<<"user_id">>, User) of
-%                                     undefined -> {ok, login_page(Req), State};
-%                                     UserID    -> {ok, play_page(UserID, Req), State}
-%                                 end
-                        end
-                end;
-            {Code, _} ->
-                {ok, play_page(Code, Req), State}
-        end
+    {ok, Req, #state{endpoint=Endpoint}}.
+
+handle(Req, State=#state{endpoint=Endpoint}) ->
+    lager:debug("Serving canvas to '~s'...", [Endpoint]),
+    try serve_canvas(Req, State) of
+        {ok, Body, NewState} ->
+            {ok, Reply} = cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], Body, Req),
+            {ok, Reply, NewState}
     catch
         error:ErrorCode when is_integer(ErrorCode) ->
             lager:error("Serving iframe to '~s': error ~B", [Endpoint, ErrorCode]),
@@ -46,6 +30,43 @@ handle(Req, State) ->
             lager:error("Serving iframe to '~s': error ~B (~s)", [Endpoint, ErrorCode, ErrorMsg]),
             {ok, Reply} = cowboy_req:reply(ErrorCode, [], ErrorMsg, Req),
             {ok, Reply, State}
+    end.
+
+serve_canvas(Req, State=#state{endpoint=Endpoint}) ->
+    case cowboy_req:method(Req) of
+        {<<"POST">>, _} -> ok;
+        _               -> error({405, <<"Method not allowed">>})
+    end,
+    case cowboy_req:has_body(Req) of
+        true  -> ok;
+        false -> error({403, <<"Missing body">>})
+    end,
+    case cowboy_req:qs_val(<<"error">>, Req) of
+        {undefined, _} -> ok;
+        {<<"access_denied">>, _} ->
+            %% TODO: Redirect somewhere else?
+            lager:debug("Serving canvas to '~s': user denied authorization", [Endpoint])
+    end,
+    {ok, PostVars, _} = cowboy_req:body_qs(Req),
+    case proplists:get_value(<<"signed_request">>, PostVars) of
+        undefined -> error({400, <<"Invalid request">>});
+        SignedRequest when is_binary(SignedRequest) ->
+            User = parse_signed_request(SignedRequest),
+            case proplists:get_value(<<"user_id">>, User) of
+                undefined ->
+                    {ok, Scope} = application:get_env(facebook_canvas, scope),
+                    {ok, AppID} = application:get_env(facebook_canvas, app_id),
+                    {ok, AppNamespace} = application:get_env(facebook_canvas, app_namespace),
+                    {ok, Content} = login_dtl:render([{app_id, AppID}, {app_namespace, AppNamespace}, {scope, Scope}]),
+                    lager:debug("Serving canvas to '~s': redirecting to authorization", [Endpoint]),
+                    {ok, Content, State};
+                UserID when is_binary(UserID) ->
+                    OAuthToken = proplists:get_value(<<"oauth_token">>, User),
+                    {ok, AppID} = application:get_env(facebook_canvas, app_id),
+                    {ok, Content} = play_dtl:render([{app_id, AppID}, {user_id, UserID}, {oauth_token, OAuthToken}]),
+                    lager:debug("Serving canvas to '~s': ok", [Endpoint]),
+                    {ok, Content, State}
+            end
     end.
 
 terminate(_Reason, _Req, _State) ->
@@ -66,37 +87,17 @@ parse_signed_request(SignedRequest) ->
     [SHA, Data] = re:split(SignedRequest, "\\."),
     Json = decode_base64url(Data),
     {User} = jiffy:decode(Json),
-    {ok, SecretKey} = application:get_env(facebook_canvas, secret_key),
+    {ok, SecretKey} = application:get_env(facebook_canvas, app_secret),
     case proplists:get_value(<<"algorithm">>, User) of
         <<"HMAC-SHA256">> ->
             ProvidedSHA = decode_base64url(SHA),
             ExpectedSHA = hmac:hmac256(SecretKey, Data),
             case ExpectedSHA =:= ProvidedSHA of
                 true  -> User;
-                false -> error(403)
+                false -> error({403, <<"Invalid signature">>})
             end;
         _ ->
-            error({400, <<"unexpected encryption algorithm">>})
+            error({400, <<"Encryption algorithm unsupported.">>})
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% login_cancelled(Req) ->
-
-
-login_page(Req) ->
-    {ok, AppID} = application:get_env(facebook_canvas, application_id),
-    {ok, RedirectURL} = application:get_env(facebook_canvas, redirect_url),
-    Template = "https://www.facebook.com/dialog/oauth?client_id=~s&redirect_uri=~s&scope=~s",
-    URL = iolist_to_binary(io_lib:format(Template, [AppID, http_uri:encode(RedirectURL), scope()])),
-    Reply = <<"<html><head><script>top.location.href = '", URL/binary, "';</script></head></html>">>,
-    cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], Reply, Req).
-
-play_page(UserID, Req) when is_list(UserID) ->
-    play_page(iolist_to_binary(UserID), Req);
-play_page(UserID, Req) when is_binary(UserID) ->
-    Reply = <<"<html><head>Hi there, ", UserID/binary, "!<br/>Here is your game, play it!</head></html>">>,
-    cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], Reply, Req).
-
-scope() ->
-    "email".
