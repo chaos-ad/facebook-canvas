82b7057d42baf8be090e9dbd6cef68f37fda4494
Anatoly Kanivetskiy <chaos.a.d88@gmail.com>
2013-04-18 12:33:15 +0200

    Host, port and listener pool made configurable

--- src/facebook_canvas_app.erl
+++ src/facebook_canvas_app.erl
@@ -9,13 +9,15 @@
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/", facebook_canvas_handler, []}
-        ]}
+        {'_', [{"/", facebook_canvas_handler, []}]}
     ]),
-    {ok, _} = cowboy:start_http(http, 1, [{ip, {0,0,0,0}},{port, 8080}], [
-        {env, [{dispatch, Dispatch}]}
-    ]),
+    {ok, Host} = application:get_env(facebook_canvas, bind_host),
+    {ok, Port} = application:get_env(facebook_canvas, bind_port),
+    {ok, Pool} = application:get_env(facebook_canvas, listeners),
+    {ok, _} = cowboy:start_http(http, Pool,
+        [{ip, Host},{port, Port}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
     facebook_canvas_sup:start_link().
 
 stop(_State) ->
